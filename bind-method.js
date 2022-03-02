06e0adfa8d1ae872a39d5d09ab2bede546bf3df9
Rafia Sultana <rafiasultana0097@gmail.com>
2022-03-02 13:03:51 +0600

    updated

--- bind-method.js
+++ bind-method.js
@@ -6,6 +6,27 @@ const kibria = {
 
     treatDey: function (expense) {
         this.money = this.money - expense;
+        console.log(this);
         return this.money;
     }
 }
+const heroAlam = {
+
+    name: 'xyz',
+    money: 6000,
+    passion: 'doing nothing',
+
+}
+const gates = {
+
+    name: 'gates_xyz',
+    money: 8000,
+    passion: 'doing everything',
+
+}
+
+kibria.treatDey(500);
+const heroTreatDey = kibria.treatDey.bind(heroAlam);
+heroTreatDey(500);
+const gatesTreatDey = kibria.treatDey.bind(gates);
+gatesTreatDey(500);
